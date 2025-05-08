9de89edad805c990551b11294edee6c1e012fee2
Musfirah Rumman <h04sareena@gmail.com>
2025-05-09 00:37:39 +0500

    Add files via upload

---
+++ ThirdProgram.py
@@ -0,0 +1,15 @@
+# Snake Case
+first_name = "Musfirah"
+middle_name = "Rumman"
+# Camel Case
+lastName = "Qureshi"
+nickName = "Jugnu"
+# Pascal Case
+UserName = "Python Programmer"
+StudentName = "Nelofar"
+print(first_name)
+print(middle_name)
+print(lastName)
+print(nickName)
+print(UserName)
+print(StudentName)
